813a1fa4125378b5c6dbded5e4b7bf51e4b18879
Parmote Wijitsarn <parmote_tab@hotmail.com>
2016-04-11 16:03:32 +0700

    test model1

--- app/Http/Controllers/Library/BookManagementController.php
+++ app/Http/Controllers/Library/BookManagementController.php
@@ -28,6 +28,26 @@ class BookManagementController extends Controller
      */
     public function create()
     {
+        $data = new BookManagement();
+        $data->book_name = 'book1';
+        $data->author = 'TestingBook';
+        $data->interpreter = 'TestingBook';
+        $data->year_publish = 2548;
+        $data->edition = 1;
+        $data->isbn = 212224236248;
+        $data->book_id = 1;
+        $data->url = 'http://book.com';
+        $data->category_id = 'a9b8c7d6e5';
+        $data->title = 'book1';
+        $data->key_word = 'Book book1';
+        $data->book_review = 'This is a test add book.';
+        $data->cd_copy = 2;
+        $data->total_time = '120:30:50';
+        $data->book_status = 'reading';
+        $data->read_by = 'TestReader';
+        $data->comment = 'No comment';
+        $data->publish_location = 1;
+        $data->save();
         return view('library.bookmanagement.create');
     }
 
